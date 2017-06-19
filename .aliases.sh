9641c4d369883423b8c25919238934d78d0d2088
Will Hall <will@willhallonline.co.uk>
2017-06-19 11:01:50 +0100

    Include node and stylelint in docker aliases

--- .aliases.sh
+++ .aliases.sh
@@ -5,6 +5,7 @@ function docker_alias() {
         ${@:2}
 }
 
+# PHP
 alias phpcs-cakephp="docker_alias /app willhallonline/cakephp-phpcs:alpine phps"
 alias phpcbf-cakephp="docker_alias /app willhallonline/cakephp-phpcs:alpine phpcbf"
 alias phpcs-wordpress="docker_alias /app willhallonline/wordpress-phpcs:alpine phpcs"
@@ -18,3 +19,11 @@ alias phpcbf-laravel="docker_alias /app willhallonline/laravel-phpcs:alpine phpc
 alias phpcs-docker="docker_alias /app willhallonline/phpcs:alpine phpcs"
 alias phpcbf-docker="docker_alias /app willhallonline/phpcs:alpine phpcbf"
 alias composer-docker="docker_alias /app willhallonline/composer:alpine composer"
+
+# Stylelint
+alias stylelint-docker="docker_alias /app willhallonline/stylelint:alpine"
+
+# Node/NPM
+alias node-docker="docker_alias /app node:alpine"
+alias npm-docker="docker_alias /app node:alpine"
+alias yarn-docker="docker_alias /app node:alpine"
