58c08e4ee6ce27778a13ed8c686adc6fa8e971ab
Will Hall <will@willhallonline.co.uk>
2017-06-20 10:55:10 +0100

    Update with extensions for Drupal and ESLint standards

--- .aliases.sh
+++ .aliases.sh
@@ -5,13 +5,12 @@ function docker_alias() {
         ${@:2}
 }
 
-# PHP
 alias phpcs-cakephp="docker_alias /app willhallonline/cakephp-phpcs:alpine phps"
 alias phpcbf-cakephp="docker_alias /app willhallonline/cakephp-phpcs:alpine phpcbf"
 alias phpcs-wordpress="docker_alias /app willhallonline/wordpress-phpcs:alpine phpcs"
 alias phpcbf-wordpress="docker_alias /app willhallonline/wordpress-phpcs:alpine phpcbf"
-alias phpcs-drupal="docker_alias /app willhallonline/drupal-phpcs:alpine phpcs"
-alias phpcbf-drupal="docker_alias /app willhallonline/drupal-phpcs:alpine phpcbf"
+alias phpcs-drupal="docker_alias /app willhallonline/drupal-phpcs:alpine phpcs --extensions=php,inc,install,module,theme"
+alias phpcbf-drupal="docker_alias /app willhallonline/drupal-phpcs:alpine phpcbf --extensions=php,inc,install,module,theme"
 alias phpcs-yii="docker_alias /app willhallonline/yii-phpcs:alpine phpcs"
 alias phpcbf-yii="docker_alias /app willhallonline/yii-phpcs:alpine phpcbf"
 alias phpcs-laravel="docker_alias /app willhallonline/laravel-phpcs:alpine phpcs"
@@ -23,7 +22,11 @@ alias composer-docker="docker_alias /app willhallonline/composer:alpine composer
 # Stylelint
 alias stylelint-docker="docker_alias /app willhallonline/stylelint:alpine"
 
-# Node/NPM
-alias node-docker="docker_alias /app node:alpine"
-alias npm-docker="docker_alias /app node:alpine"
-alias yarn-docker="docker_alias /app node:alpine"
+# ESLint
+alias eslint-standard="docker_alias /app willhallonline/eslint-standard:alpine"
+alias eslint-airbnb="docker_alias /app willhallonline/eslint-airbnb:alpine"
+
+# JavaScript / CoffeeScript
+alias node-docker="docker_alias ./ node:alpine node"
+alias npm-docker="docker_alias ./ node:alpine npm"
+alias coffee-docker="docker_alias ./ shouldbee/coffeescript coffee"
